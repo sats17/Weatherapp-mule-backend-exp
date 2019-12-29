8172f8579319b9d4e46bfd199e615ffb813aff1e
sats17 <satishkumbhar9999@gmail.com>
2019-12-29 14:45:23 +0530

    Added weather api and api key validation

---
+++ src/main/java/com/mule/exceptions/AuthenticationException.java
@@ -0,0 +1,38 @@
+package com.mule.exceptions;
+
+public class AuthenticationException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AuthenticationException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AuthenticationException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AuthenticationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AuthenticationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AuthenticationException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
